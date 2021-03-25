dc9f4a57676e7147322b05f807764a4323018c9f
Pedro Zier <41027931+pedrozier@users.noreply.github.com>
2021-03-25 10:42:59 -0300

    Add files via upload

---
+++ lcd1.ino
@@ -0,0 +1,15 @@
+#include <LiquidCrystal.h>
+LiquidCrystal lcd(2,3,4,6,7,8,9,10,11,12,13);
+void setup()
+{
+lcd.begin(16,2);
+}
+
+void loop()
+{
+  lcd.clear();
+  lcd.print("hello world");
+  lcd.setCursor(0,2);
+  lcd.print("test ok");
+    delay(500); // Delay a little bit to improve simulation performance
+}

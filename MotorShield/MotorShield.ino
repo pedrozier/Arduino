aeac220f9f3fa8f4208ae5cf67a28bfd72c09c5b
Pedro Zier <41027931+pedrozier@users.noreply.github.com>
2021-11-07 22:07:00 -0300

    Add files via upload

---
+++ MotorShield/MotorShield.ino
@@ -0,0 +1,34 @@
+#include <AFMotor.h>
+
+AF_DCMotor motor1(1);
+AF_DCMotor motor2(2);
+AF_DCMotor motor3(3);
+AF_DCMotor motor4(4);
+void setup() {
+  motor1.setSpeed(0);// 0 a 255
+  motor2.setSpeed(0);
+  motor3.setSpeed(0);
+  motor4.setSpeed(0);
+}
+
+void loop() {
+  motor1.setSpeed(255);
+  motor1.run(FORWARD);
+  motor2.setSpeed(255);
+  motor2.run(FORWARD);
+  motor3.setSpeed(255);
+  motor3.run(FORWARD);
+  motor4.setSpeed(255);
+  motor4.run(FORWARD);
+delay(2000);
+  motor1.setSpeed(0);
+  motor1.run(RELEASE);
+  motor2.setSpeed(0);
+  motor2.run(RELEASE);
+  motor3.setSpeed(0);
+  motor3.run(RELEASE);
+  motor4.setSpeed(0);
+  motor4.run(RELEASE);
+
+
+}
